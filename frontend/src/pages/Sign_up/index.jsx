63858884feb0aed10b0e8f016086a93180bcbf91
Wescley J. <wescleyjnavarro@gmail.com>
2026-07-17 17:15:55 -0300

    chore: aprimora o gitignore

---
+++ frontend/src/pages/Sign_up/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles.css'
+
+function Index() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Index
